2b6e5ed0e3df75848671b60759877a486dada434
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2026-03-12 13:00:57 +0600

    refactor: extract PlexusPulseApp to app.dart and clean up unused files

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlexusPulseApp extends StatelessWidget {
+  const PlexusPulseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
