e4e277beea7f1fa18fe9dcd5b38b02d28cb8f02c
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2026-03-12 13:21:57 +0600

    feat: setup app theme with clean architecture

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'core/theme/app_theme.dart';
+
 class PlexusPulseApp extends StatelessWidget {
   const PlexusPulseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Plexus Pulse',
+      theme: AppTheme.lightTheme,
+      home: const Scaffold(
+        body: Center(
+          child: Text('Plexus Pulse Initialization...'),
+        ),
+      ),
+    );
   }
 }
